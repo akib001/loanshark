8d020bc3491a7fba3ae6f79e4f9f34a0492cd3cf
Akib Ahmed <akibahmed2011@gmail.com>
2022-10-28 00:10:34 +0600

    1st commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,21 @@
-import '../styles/globals.css'
+import ProgressBar from '@badrap/bar-of-progress';
+import Router from 'next/router';
+import Layout from "../components/Layout";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+const progress = new ProgressBar({
+    size: 4, color: '#FE595E', className: 'z-50', delay: 100,
+});
+
+// Router.events.on whenever there is a change on router it dispatches a action
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
+
+
+function MyApp({Component, pageProps}) {
+    return <Layout>
+        <Component {...pageProps} />
+    </Layout>
 }
 
 export default MyApp
